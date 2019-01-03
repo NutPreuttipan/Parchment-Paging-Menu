da9a2d2816d6ab893992bb0ccf4b2e4bea78419e
Preuttipan <terrorize.nut@gmail.com>
2019-01-03 14:02:12 +0700

    initial commit

---
+++ PageMenu/View2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  View2ViewController.swift
+//  PageMenu
+//
+//  Created by Preuttipan Janpen on 3/1/2562 BE.
+//  Copyright © 2562 Lphant Solutions. All rights reserved.
+//
+
+import UIKit
+
+class View2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
