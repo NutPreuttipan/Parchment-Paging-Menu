78cc055630353e04ee6628192fffdce6f3064945
Preuttipan <terrorize.nut@gmail.com>
2019-01-04 10:52:32 +0700

    handle with datasource

--- PageMenu/View1ViewController.swift
+++ PageMenu/View1ViewController.swift
@@ -12,18 +12,11 @@ class View1ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("page1")
+        print("page1 working...")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onClickAction(_ sender: Any) {
+        print("Action page1")
     }
-    */
 
 }
--- PageMenu/ViewController.swift
+++ PageMenu/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Parchment
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PagingViewControllerDataSource {
     
     
     
@@ -17,21 +17,42 @@ class ViewController: UIViewController {
     
     let pagingTitle = ["Page1","Page2"]
     
-    let viewPageMenuController = [View1ViewController(),View2ViewController()]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        handleWithFixedPagingViewController()
+        handleWithDataSource()
+    }
+    
+    func numberOfViewControllers<T>(in pagingViewController: PagingViewController<T>) -> Int where T : PagingItem, T : Comparable, T : Hashable {
+        return 2
+    }
+
+    func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, viewControllerForIndex index: Int) -> UIViewController where T : PagingItem, T : Comparable, T : Hashable {
         
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let firstViewController = storyboard.instantiateViewController(withIdentifier: "page1") as! View1ViewController
+        let secondViewController = storyboard.instantiateViewController(withIdentifier: "page2") as! View2ViewController
+        let viewPageMenuController = [firstViewController,secondViewController]
+        
+        return viewPageMenuController[index]
+    }
+
+    func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, pagingItemForIndex index: Int) -> T where T : PagingItem, T : Comparable, T : Hashable {
+        return PagingIndexItem(index: index, title: pagingTitle[index]) as! T
+    }
+    
+    
+    func handleWithFixedPagingViewController() {
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let firstViewController = storyboard.instantiateViewController(withIdentifier: "page1") as! View1ViewController
         let secondViewController = storyboard.instantiateViewController(withIdentifier: "page2") as! View2ViewController
         
         let pagingViewController = FixedPagingViewController(viewControllers: [
-            firstViewController,
-            secondViewController
-            ])
+                    firstViewController,
+                    secondViewController
+                    ])
         
         pagingViewController.textColor = UIColor.red
         pagingViewController.selectedTextColor = UIColor.blue
@@ -42,30 +63,37 @@ class ViewController: UIViewController {
         viewPageMenu.addSubview(pagingViewController.view)
         pagingViewController.didMove(toParent: self)
         pagingViewController.view.translatesAutoresizingMaskIntoConstraints = false
-
+        
         NSLayoutConstraint.activate([
             pagingViewController.view.leadingAnchor.constraint(equalTo: viewPageMenu.leadingAnchor),
             pagingViewController.view.trailingAnchor.constraint(equalTo: viewPageMenu.trailingAnchor),
             pagingViewController.view.bottomAnchor.constraint(equalTo: viewPageMenu.bottomAnchor),
             pagingViewController.view.topAnchor.constraint(equalTo: viewPageMenu.topAnchor)
             ])
-
-//        let pagingViewController = PagingViewController<PagingIndexItem>()
-//        pagingViewController.dataSource = self
-      
     }
     
-//    func numberOfViewControllers<T>(in pagingViewController: PagingViewController<T>) -> Int where T : PagingItem, T : Comparable, T : Hashable {
-//        return 2
-//    }
-//
-//    func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, viewControllerForIndex index: Int) -> UIViewController where T : PagingItem, T : Comparable, T : Hashable {
-//        return viewPageMenuController[index]
-//    }
-//
-//    func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, pagingItemForIndex index: Int) -> T where T : PagingItem, T : Comparable, T : Hashable {
-//        return PagingIndexItem(index: index, title: pagingTitle[index]) as! T
-//    }
-
+     func handleWithDataSource() {
+        
+        // must implement datasource and 3 method above
+        let pagingViewController = PagingViewController<PagingIndexItem>()
+        pagingViewController.dataSource = self
+        
+        pagingViewController.textColor = UIColor.red
+        pagingViewController.selectedTextColor = UIColor.blue
+        pagingViewController.borderColor = UIColor.purple
+        pagingViewController.indicatorColor = UIColor.yellow
+        
+        addChild(pagingViewController)
+        viewPageMenu.addSubview(pagingViewController.view)
+        pagingViewController.didMove(toParent: self)
+        pagingViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            pagingViewController.view.leadingAnchor.constraint(equalTo: viewPageMenu.leadingAnchor),
+            pagingViewController.view.trailingAnchor.constraint(equalTo: viewPageMenu.trailingAnchor),
+            pagingViewController.view.bottomAnchor.constraint(equalTo: viewPageMenu.bottomAnchor),
+            pagingViewController.view.topAnchor.constraint(equalTo: viewPageMenu.topAnchor)
+            ])
+    }
 }
 
